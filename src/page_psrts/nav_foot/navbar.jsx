1648aa60ddf32b0f9a96c2bc12565ba479fc767e
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-04-19 10:43:21 +0500

    entrance to navbar

---
+++ src/page_psrts/nav_foot/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../../App.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="container"></div>
+    </div>
+  );
+}
+
+function Links (){
+    return (
+        <>
+            
+        </>
+    )
+}
